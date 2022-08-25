5dc714c55ee3c9291ebc3910ee307fdfedd8b4dd
Kseniya <kz@atommobility.com>
2022-08-26 00:33:26 +0600

    Add Veriff SDK

--- ClientApp/ViewController.swift
+++ ClientApp/ViewController.swift
@@ -6,14 +6,89 @@
 //
 
 import UIKit
+import Veriff
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var faceTitleLabel: UILabel!
+    @IBOutlet weak var faceStatusLabel: UILabel!
+    @IBOutlet weak var faceErrorLabel: UILabel!
+    
+    @IBOutlet weak var documentTitileLabel: UILabel!
+    @IBOutlet weak var documentStatusLabel: UILabel!
+    @IBOutlet weak var documentErrorLabel: UILabel!
+    
+    var results: Veriff.Results?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setUpUI()
     }
-
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        guard results == results else { return }
+        
+        Veriff.shared.delegate = self
+        Veriff.shared.start(from: self)
+    }
+    
+    func setUpUI() {
+        view.backgroundColor = .white
+    }
+    
+    func showResults(_ results: Veriff.Results) {
+        switch results.faceRecognitionResult {
+        case.success:
+            faceErrorLabel.isHidden = true
+            faceStatusLabel.text = "Success"
+            faceStatusLabel.textColor = .green
+        case .failure(let error):
+            faceErrorLabel.isHidden = false
+            faceStatusLabel.text = "Falure"
+            faceStatusLabel.textColor = .red
+            faceErrorLabel.text = error.errorText
+        }
+        
+        switch results.textRecognitionResult {
+        case.success:
+            documentErrorLabel.isHidden = true
+            documentStatusLabel.text = "Success"
+            documentStatusLabel.textColor = .green
+        case .failure(let error):
+            documentErrorLabel.isHidden = false
+            documentStatusLabel.text = "Falure"
+            documentStatusLabel.textColor = .red
+            documentErrorLabel.text = error.errorText
+        }
+    }
+    
+    // MARK: - Actions
+    
+    @IBAction func tryAgainAction(_ sender: Any) {
+        Veriff.shared.start(from: self)
+    }
 }
 
+extension ViewController: Veriff.Delegate {
+    func didFinish(results: Veriff.Results) {
+        self.results = results
+        showResults(results)
+    }
+}
+
+extension Veriff.ErrorReason {
+    
+    var errorText: String {
+        switch self {
+        case .noFace:
+            return "No face found"
+        case .moreThanOneFace:
+            return "Found more than one face"
+        case .noText:
+            return "No text found"
+        case .internalError:
+            return "Internal Error"
+        }
+    }
+}
